9e1f6dcfd7996abbac43d38a1d3ab3d44f6b7c43
archanajyothy <archanajyothy@gmail.com>
2023-05-27 02:39:04 +0530

    Authentication

--- server/server.js
+++ server/server.js
@@ -5,10 +5,16 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
-app.get('/',(req,res)=>{
+//Routes
+app.get("/", (req,res)=>{
     res.json({msg:"Hello"})
 })
+
+app.use('/api', require('./routes/authRouter'))
 
 const URI = process.env.MONGODB_URL
 mongoose.connect(URI, {
