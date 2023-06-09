7501c75120edf227a54bbe480304132f6e4e3e81
archanajyothy <archanajyothy@gmail.com>
2023-06-09 12:26:01 +0530

    Follow[2]

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -2,23 +2,27 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Avatar from './Avatar'
 
-const UserCard = ({user, border, handleClose}) => {
+const UserCard = ({children, user, border, handleClose, setShowFollowers, setShowFollowing}) => {
   const handleCloseAll = () => {
       if (handleClose) handleClose()
+      if (setShowFollowers) setShowFollowers(false)
+      if (setShowFollowing) setShowFollowing(false)
 
   }
   return (
-    <div className={`d-flex p-2 align-item-center ${border}`}>
+    <div className={`d-flex p-2 align-items-center justify-content-between ${border}`}>
       <div>
         <Link to={`/profile/${user._id}`} onClick={handleCloseAll}
-        className="d-flex align-item-center" >
-            <Avatar sec={user.avatar} size="big-avatar" />
+        className="d-flex align-items-center" >
+            <Avatar src={user.avatar} size="big-avatar" />
             <div className='ml-1'style={{transform: 'translateY(-2px)'}}>
                 <span className='d-block'>{user.username}</span>
                 <small style={{opacity: 0.7}}>{user.fullname}</small>
             </div>
         </Link>
       </div>
+
+      {children}
         
     </div>
   )
--- client/src/redux/actions/profileAction.js
+++ client/src/redux/actions/profileAction.js
@@ -75,7 +75,18 @@ export const updateProfileUser = ({userData, avatar, auth}) => async (dispatch) 
 }
 
 export const follow = ({ users, user, auth }) => async (dispatch) => {
-    let newUser = {...user, followers: [...user.followers, auth.user]}
+    let newUser;
+    
+    if (users.every(item => item._id !== user._id)) {
+        newUser = {...user, followers: [...user.followers, auth.user]}
+    } else {
+        users.forEach(item => {
+            if(item._id === user._id){
+                newUser = {...item, followers: [...item.followers, auth.user]}
+            }
+        })
+    }
+    
     
     dispatch({ type: PROFILE_TYPES.FOLLOW, payload: newUser })
 
@@ -86,14 +97,28 @@ export const follow = ({ users, user, auth }) => async (dispatch) => {
             user: {...auth.user, following: [...auth.user.following, newUser]}
         }
     })
+    try {
+        await patchDataAPI(`user/${user._id}/follow`, null, auth.token)
+    } catch (err) {
+        dispatch({
+            type: GLOBALTYPES.ALERT,
+            payload: {error: err.response.data.msg}
+        }) 
+    }
 }
 
 export const unfollow = ({ users, user, auth }) => async (dispatch) => {
-    let newUser = {
-        ...user, 
-        followers: DeleteData(user.followers, auth.user._id ) 
+    let newUser;
+
+    if (users.every(item => item._id !== user._id)) {
+        newUser = {...user, followers: DeleteData(user.followers, auth.user._id)}
+    } else {
+        users.forEach(item => {
+            if(item._id === user._id){
+                newUser = {...item, followers: DeleteData(item.followers, auth.user._id)}
+            }
+        })
     }
-    console.log(newUser);
 
     dispatch({ type: PROFILE_TYPES.UNFOLLOW, payload: newUser })
 
@@ -106,4 +131,13 @@ export const unfollow = ({ users, user, auth }) => async (dispatch) => {
                 following:  DeleteData(auth.user.following, newUser._id) }
         }
     })
+
+    try {
+        await patchDataAPI(`user/${user._id}/unfollow`, null, auth.token)
+    } catch (err) {
+        dispatch({
+            type: GLOBALTYPES.ALERT,
+            payload: {error: err.response.data.msg}
+        }) 
+    }
 }
