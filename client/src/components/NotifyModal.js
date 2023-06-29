36bc2f78b2d588ffab38d73b9a877ad993050386
archanajyothy <archanajyothy@gmail.com>
2023-06-29 19:16:34 +0530

    Delete all notifies and added notification sound

--- client/src/components/NotifyModal.js
+++ client/src/components/NotifyModal.js
@@ -4,10 +4,29 @@ import NoNotice from '../images/notice.png'
 import { Link } from 'react-router-dom'
 import Avatar from './Avatar'
 import moment from 'moment'
+import { isReadNotify, NOTIFY_TYPES, deleteAllNotifies } from '../redux/actions/notifyAction'
 
 const NotifyModal = () => {
   const { auth, notify } = useSelector(state => state)
   const dispatch = useDispatch()
+
+  const handleIsRead = (msg) => {
+    dispatch(isReadNotify({msg, auth}))
+  }
+
+  const handleSound = (msg) => {
+      dispatch({type: NOTIFY_TYPES.UPDATE_SOUND, payload: !notify.sound})
+  }
+
+  const handleDeleteAll = () => {
+    const newArr = notify.data.filter(item => item.isRead === false)
+    if(newArr.length === 0) return dispatch(deleteAllNotifies(auth.token))
+
+    if(window.confirm(`You have ${newArr.length} unread notices. Are you want to delete all?`)){
+        return dispatch(deleteAllNotifies(auth.token))
+    }
+  }
+
   return (
     <div style={{minWidth: '280px'}}>
       <div className='d-flex justify-content-between align-items-center px-3'>
@@ -15,9 +34,11 @@ const NotifyModal = () => {
         {
           notify.sound
           ? <i className='fas fa-bell text-danger' 
-          style={{fontSize: '1.2rem', cursor:'pointer'}} />
+          style={{fontSize: '1.2rem', cursor:'pointer'}} 
+          onClick= {handleSound} />
           : <i className='fas fa-bell-slash text-danger' 
-          style={{fontSize: '1.2rem', cursor:'pointer'}} />
+          style={{fontSize: '1.2rem', cursor:'pointer'}} 
+          onClick={handleSound} />
         }
       </div>
       <hr className='mt-0'/>
@@ -29,7 +50,8 @@ const NotifyModal = () => {
         {
           notify.data.map((msg, index) => (
             <div key={index} className='px-2 mb-3' >
-                <Link to={`${msg.url}`} className='d-flex text-dark align-items-center'>
+                <Link to={`${msg.url}`} className='d-flex text-dark align-items-center' 
+                onClick={() => handleIsRead(msg)}>
                     <Avatar src={msg.user.avatar} size="big-avatar" />
 
                     <div className='mx-1 flex-fill'>
@@ -54,7 +76,8 @@ const NotifyModal = () => {
         }
       </div>
       <hr className='my-1'/>
-      <div className='text-right text-danger mr-2' style={{cursor: 'pointer'}}>
+      <div className='text-right text-danger mr-2' style={{cursor: 'pointer'}} 
+      onClick={handleDeleteAll}>
           Delete All
       </div>
 
