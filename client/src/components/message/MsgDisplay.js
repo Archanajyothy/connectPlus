365df6c00533e3ce5e87d5da5c04830a9d63a8ad
archanajyothy <archanajyothy@gmail.com>
2023-07-07 12:34:01 +0530

    Delete messages

--- client/src/components/message/MsgDisplay.js
+++ client/src/components/message/MsgDisplay.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Avatar from '../Avatar'
 import { imageShow, videoShow } from '../../utils/mediaShow'
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteMessages } from '../../redux/actions/messageAction'
 
-const MsgDisplay = ({user, msg, theme}) => {
-    
+const MsgDisplay = ({user, msg, theme, data}) => {
+    const { auth } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const handleDeleteMessages = () => {
+      if(data){
+        dispatch(deleteMessages({msg, data, auth}))
+      }
+    }
+
   return (
     <>
         <div className='chat title'>
@@ -12,24 +22,31 @@ const MsgDisplay = ({user, msg, theme}) => {
         </div>
 
         <div className='you_content'>
-            { 
-              msg.text && 
-              <div className='chat_text'
-              style={{filter: theme? 'invert(1)' : 'invert(0)'}}>
-                {msg.text}
-              </div>
-            }
             {
-              msg.media.map((item, index) => (
-                <div key={index}>
-                  {
-                    item.url.match(/video/i)
-                    ? videoShow(item.url, theme)
-                    : imageShow(item.url, theme)
-                  }
-                </div>
-              ))
+                user._id === auth.user._id && 
+                <i className='fas fa-trash text-danger' 
+                onClick={handleDeleteMessages} />
             }
+            <div>
+                { 
+                  msg.text && 
+                  <div className='chat_text'
+                  style={{filter: theme? 'invert(1)' : 'invert(0)'}}>
+                    {msg.text}
+                  </div>
+                }
+                {
+                  msg.media.map((item, index) => (
+                    <div key={index}>
+                      {
+                        item.url.match(/video/i)
+                        ? videoShow(item.url, theme)
+                        : imageShow(item.url, theme)
+                      }
+                    </div>
+                  ))
+                }
+            </div>
         </div>
         
 
